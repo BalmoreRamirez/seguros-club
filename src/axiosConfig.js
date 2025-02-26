08201f9e6bac939dd575de8a481a3815851c6a10
Balmore Ramirez <jbramirez@innovacion.gob.sv>
2025-02-26 09:18:11 -0600

    page error

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-  baseURL: 'https://seguros-aps-back.onrender.com/api', // Replace with your API base URL
+  baseURL: 'https://seguros-aps-back.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
